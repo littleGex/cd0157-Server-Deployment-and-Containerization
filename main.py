a80b5b05ac34956dd444eb65e0d6fb913f7782e8
littleGex <67484931+littleGex@users.noreply.github.com>
2022-06-17 21:05:03 +0200

    Updates to configurations

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 
 def _logger():
-    '''
+    """
     Setup logger format, level, and handler.
 
-    RETURNS: log object
-    '''
+    :returns: log object
+    """
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger(__name__)
@@ -35,8 +35,9 @@ def _logger():
 
 
 LOG = _logger()
-LOG.debug("Starting with log level: %s" % LOG_LEVEL )
+LOG.debug("Starting with log level: %s" % LOG_LEVEL)
 APP = Flask(__name__)
+
 
 def require_jwt(function):
     """
@@ -44,13 +45,13 @@ def require_jwt(function):
     """
     @functools.wraps(function)
     def decorated_function(*args, **kws):
-        if not 'Authorization' in request.headers:
+        if 'Authorization' not in request.headers:
             abort(401)
         data = request.headers['Authorization']
         token = str.replace(str(data), 'Bearer ', '')
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except: # pylint: disable=bare-except
+        except:  # pylint: disable=bare-except
             abort(401)
 
         return function(*args, **kws)
@@ -88,19 +89,18 @@ def decode_jwt():
     """
     Check user token and return non-secret data
     """
-    if not 'Authorization' in request.headers:
+    if 'Authorization' not in request.headers:
         abort(401)
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+    except:  # pylint: disable=bare-except
         abort(401)
-
 
     response = {'email': data['email'],
                 'exp': data['exp'],
-                'nbf': data['nbf'] }
+                'nbf': data['nbf']}
     return jsonify(**response)
 
 
@@ -110,6 +110,7 @@ def _get_jwt(user_data):
                'nbf': datetime.datetime.utcnow(),
                'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
